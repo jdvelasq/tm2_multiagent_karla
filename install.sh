f317550e730ddf76e2810de29e7f4c416c9b6115
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-06-25 13:11:20 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install --upgrade pip
+pip3 install jupyterlab
+pip3 install duckdb graphviz
+pip3 install jupyterlab-code-formatter
+pip3 install black isort
+pip3 install -e /Volumes/GitHub/techminer2 --force-reinstall
+python3 -m textblob.download_corpora
